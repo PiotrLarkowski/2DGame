445a6abe0d382d8be286cbae1333e5a98dee215c
PiotrLarkowski <p.larkowski90@gmail.com>
2023-07-18 20:57:28 +0200

    Add Thread to our game

--- src/main/java/org/example/MainJPanel.java
+++ src/main/java/org/example/MainJPanel.java
@@ -3,7 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainJPanel extends JPanel {
+public class MainJPanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
     final int scale = 3;
 
@@ -13,9 +13,21 @@ public class MainJPanel extends JPanel {
     final int screenWidth = finalSize * maxScreenCol;
     final int screenHeight = finalSize * maxScreenRow;
 
+    Thread mainThread;
+
     public MainJPanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        mainThread = new Thread(this);
+        mainThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
