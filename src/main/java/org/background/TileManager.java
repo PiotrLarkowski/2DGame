6e9ed322f626e3427b3e28d186d342672eacab5c
PiotrLarkowski <p.larkowski90@gmail.com>
2023-07-19 14:23:04 +0200

    Add load map

--- src/main/java/org/background/TileManager.java
+++ src/main/java/org/background/TileManager.java
@@ -4,40 +4,76 @@ import org.example.MainJPanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     MainJPanel gp;
     Tile[] tile;
-    public TileManager(MainJPanel mainJPanel){
+
+    int mapTileNumber[][];
+
+    public TileManager(MainJPanel mainJPanel) {
         this.gp = mainJPanel;
         tile = new Tile[10];
         getTileImages();
+        mapTileNumber = new int[gp.maxScreenCol][gp.maxScreenRow];
+        loadMap();
     }
-    public void getTileImages(){
-        try{
-            tile[0]=new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/backgrounds/flor.png"));
-            tile[1]=new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/backgrounds/flor.png"));
-        }catch (Exception e){
+
+    public void loadMap() {
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            int col = 0;
+            int row = 0;
+            while(col<gp.maxScreenRow && row<gp.maxScreenRow){
+                String line = bufferedReader.readLine();
+                while (col<gp.maxScreenCol){
+                    String number[] = line.split(" ");
+                    int num = Integer.parseInt(number[col]);
+                    mapTileNumber[col][row] = num;
+                    col++;
+                }
+                if(col==gp.maxScreenCol){
+                    col = 0;
+                    row++;
+                }
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void draw(Graphics2D g2){
+
+    public void getTileImages() {
+        try {
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/backgrounds/flor.png"));
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/backgrounds/wall.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
         int col = 0;
-        int row =0;
+        int row = 0;
         int x = 0;
         int y = 0;
-         while(col <gp.maxScreenCol && row < gp.maxScreenRow){
-             g2.drawImage(tile[0].image,x,y,gp.finalSize,gp.finalSize,null);
-             col++;
-             x += gp.finalSize;
-             if(col == gp.maxScreenCol){
-                 col = 0;
-                 x = 0;
-                 row++;
-                 y+=gp.finalSize;
-             }
-         }
+        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+            int tileNum = mapTileNumber[col][row];
+            g2.drawImage(tile[tileNum].image, x, y, gp.finalSize, gp.finalSize, null);
+            col++;
+            x += gp.finalSize;
+            if (col == gp.maxScreenCol) {
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.finalSize;
+            }
+        }
     }
 }
