de78ff58748872a5b9fe499737a39ec0e283016b
PiotrLarkowski <p.larkowski90@gmail.com>
2023-07-29 22:40:08 +0200

    Update map

--- src/main/java/org/background/TileManager.java
+++ src/main/java/org/background/TileManager.java
@@ -14,11 +14,11 @@ public class TileManager {
     MainJPanel gp;
     public Tile[] tile;
 
-    public int mapTileNumber[][];
+    public static int[][] mapTileNumber;
 
     public TileManager(MainJPanel mainJPanel) {
         this.gp = mainJPanel;
-        tile = new Tile[30];
+        tile = new Tile[300];
         getTileImages();
         mapTileNumber = new int[gp.maxWorldCol][gp.maxWorldRow];
         mapTileNumber = loadMap("/maps/worldMap01.txt");
@@ -35,15 +35,15 @@ public class TileManager {
                 while (col < gp.maxWorldCol) {
                     String number[] = line.split(" ");
                     int num = Integer.parseInt(number[col]);
-                    if(num == 0){
+                    if (num == 0) {
                         int i = gp.rand.nextInt(100) + 1;
-                        if(i<25&&i>=1){
+                        if (i < 25 && i >= 1) {
                             num = 0;
-                        }else if(i<50&&i>=25){
+                        } else if (i < 50 && i >= 25) {
                             num = 1;
-                        }else if(i<75&&i>=50){
+                        } else if (i < 75 && i >= 50) {
                             num = 2;
-                        }else if(i<100&&i>=75){
+                        } else if (i < 100 && i >= 75) {
                             num = 2;
                         }
                     }
@@ -59,54 +59,45 @@ public class TileManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < gp.maxWorldCol; i++) {
-            for (int j = 0; j < gp.maxWorldRow; j++) {
-                if(mapTileNumber[i][j]==1){
-                    if(i==0 && mapTileNumber[i+1][j]==1 && mapTileNumber[i][j+1] == 1){
-
-                    }
-                }
-            }
-
-        }
         return (mapTileNumber);
     }
 
     public void getTileImages() {
-        String[] paths= {
-                "/backgrounds/brickFlor/flor1",
+        String[] paths = {
+                "/backgrounds/brickFlor/flor1",//0
                 "/backgrounds/brickFlor/flor2",
                 "/backgrounds/brickFlor/flor3",
-                "/backgrounds/brickFlor/flor4",
+                "/backgrounds/brickFlor/flor4",//3
                 "/backgrounds/walls/center",
-                "/backgrounds/walls/downleftup",
+                "/backgrounds/walls/downleftup",//5
                 "/backgrounds/walls/downright",
-                "/backgrounds/walls/downrightup",
+                "/backgrounds/walls/downrightup",//7
                 "/backgrounds/walls/downup",
-                "/backgrounds/walls/leftdown",
+                "/backgrounds/walls/leftdown",//9
                 "/backgrounds/walls/leftdownright",
-                "/backgrounds/walls/leftdownrightup",
+                "/backgrounds/walls/leftdownrightup",//11
                 "/backgrounds/walls/leftright",
-                "/backgrounds/walls/leftup",
+                "/backgrounds/walls/leftup",//12
                 "/backgrounds/walls/upright",
-                "/backgrounds/walls/uprightleft"
+                "/backgrounds/walls/uprightleft"//13
         };
         boolean[] collisionArray =
-                {false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true};
+                {false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true};
         for (int i = 0; i < paths.length; i++) {
-            loadResources(i,paths[i],collisionArray[i]);
+            loadResources(i, paths[i], collisionArray[i]);
         }
     }
 
-    private void loadResources(int i,String path, boolean collision){
+    private void loadResources(int i, String path, boolean collision) {
         try {
-            tile[i]=new Tile();
+            tile[i] = new Tile();
             tile[i].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + ".png")));
             tile[i].collision = collision;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
     public void draw(Graphics2D g2) {
         int worldCol = 0;
         int worldRow = 0;
@@ -118,9 +109,9 @@ public class TileManager {
             int screenX = worldX - gp.player.worldX + gp.player.screenX;
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
             if (worldX + gp.finalSize > gp.player.worldX - gp.player.screenX &&
-                    worldX - gp.finalSize< gp.player.worldX + gp.player.screenX &&
-                    worldY + gp.finalSize> gp.player.worldY - gp.player.screenY &&
-                    worldY - gp.finalSize< gp.player.worldY + gp.player.screenY) {
+                    worldX - gp.finalSize < gp.player.worldX + gp.player.screenX &&
+                    worldY + gp.finalSize > gp.player.worldY - gp.player.screenY &&
+                    worldY - gp.finalSize < gp.player.worldY + gp.player.screenY) {
                 g2.drawImage(tile[tileNum].image, screenX, screenY, gp.finalSize, gp.finalSize, null);
             }
             worldCol++;
